a453ab4b5c44ef8fa56aeef16bedc4b0821c49e3
southpenguin <southpenguin@gmail.com>
2014-11-05 15:19:05 -0500

    Update Database.sql

--- Database.sql
+++ Database.sql
@@ -1,4 +1,4 @@
-CREATE TABLE User (
+create table User (
 	uid int(10),
 	ufname varchar(20),
 	ulname varchar(20),
@@ -6,3 +6,13 @@ CREATE TABLE User (
 	cphone varchar(10),
 	uname(ufname + " " + ulname),
 	primary key (uid));
+
+create table Art (
+	aid int(10),
+	afname varchar(20),
+	alname varchar(20),
+	aemail varchar(40),
+	asite varchar(100),
+	aname(ufname + " " + ulname),
+	primary key (aid)
+);
