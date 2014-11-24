fa3586e1b7eeff36dba6a8939d7a7819b93ecf7d
southpenguin <southpenguin@gmail.com>
2014-11-24 04:29:36 -0500

    Create SampleData.sql

---
+++ SampleData.sql
@@ -0,0 +1,30 @@
+
+-- User
+insert into user values('aaa','123456','Peter','Allen','P.Allen@gmail.com','New York','6461234567');
+insert into user values('tomlee','654321','Tom','Lee','tomlee@yahoo.com','New York','7181234567');
+insert into user values('lovemusic','lovemusic','Alice','Julia','lovemusic@gmail.com','Brooklyn','7185455678');
+insert into user values('user','1234567','Aaron','Joe','joe123@aol.com','Queens','718378265');
+insert into user values('alex123','7654321','Alexander','Thomas','alextom@nyu.edu','New York','7185639825');
+insert into user values('westcoast','wcoast','Allen','James','westcoast@gmail.com','Los Angeles','3267548264');
+insert into user values('catcat','catcat','Catty','Petty','cptty@gmail.com','Los Angeles','3267149478');
+insert into user values('dogdog','doggy123','Dom','Kevin','domkevin@gmail.com','Boston','6738261749');
+
+-- Arts
+insert into art values('linkinpark','12345678','Linkin Park','admin@linkinpark.com','www.linkinpark.com');
+insert into art values('jblunt','12345678','James Blunt','jblant@gmail.com','www.jamesblunt.com');
+insert into art values('lenka','12345678','Lenka','lenka@sina.com','www.lenkamusic.com');
+insert into art values('eminem123','12345678','Eminem','eminem@eminem.com','www.eminem.com');
+insert into art values('2hrose','12345678','Second Hand Roses','ershoumeigui@sina.com','http://site.douban.com/ershoumeigui/');
+insert into art values('sum14','12345678','Sum41','sum41@myspace.com','www.sum41.com');
+insert into art values('oasis1990','12345678','Oasis','oasis1990@gmail.com','www.oasis.com');
+insert into art values('backdoor','12345678','Back Door','backdoor@gmail.com','www.facebbok.com/backdoor');
+
+-- Location
+insert into location values('0001','Barkley Center',620,'Atlantic Avenue',NULL,'Brooklyn','NY','11217');
+insert into location values('0002','The Way Station',683,'Washington Ave',NULL,'Brooklyn','NY','11238');
+insert into location values('0003','Barbès',376,'9th St',NULL,'Brooklyn','NY','11215');
+insert into location values('0004','Fat Cat',75,'Christopher St',NULL,'New York','NY','10014');
+insert into location values('0005','Bar Nine',807,'9th Ave',NULL,'New York','NY','10019');
+insert into location values('0006','Paddy Reillys',519,'2nd Ave',NULL,'New York','NY','10016');
+insert into location values('0007','Terraza 7',4019,'Gleane St',NULL,'Queens','NY','11373');
+insert into location values('0008','Blue Whale Bar',123,'Astronaut E S Onizuka St',NULL,'Los Angeles','CA','90012');
