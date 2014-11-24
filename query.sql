4ea6571eedc6070d8715952af9d8ca6ca929de92
southpenguin <southpenguin@gmail.com>
2014-11-24 16:01:34 -0500

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,41 +1,71 @@
--- When a user signing up
+/*
+* When a user signing up, create a new tuple in User talbe, and insert the following information into the table
+* timestamp is the time when the user signed up
+*/
 INSERT INTO User
 VALUES('username','password','Firstname','Lastname','Email@Address','City','10digitNo.', timestamp);
 
-
--- When an artist signing up
+/*
+* When an artist signing up, create a new tuple in Art talbe, and insert the following information into the table
+* timestamp is the time when the artist signed up
+*/
 INSERT INTO Art
 VALUES('artusername','password','artistname','Email@Address','artistlink', timestamp);
 
-
--- When a user following someother user
+/*
+* When a user following someother user
+* followeeid is the user who is being followed
+* followerid is the user who is following
+* timestamp is the time when the follower started to follow ther followee
+*/
 INSERT INTO Follow
 VALUES('followeeid','followerid', timestamp);
 
-
--- When a user follow an artist
+/*
+* When a user follow an artist, a new tuple will be created in the Fans table
+*/
 INSERT INTO Fans
 VALUES('userid','artid', timestamp);
 
-
--- Artist create a concert
+/*
+* When an artist creates a concert, a new tuple will be created in teh Concert table and the following information will be added
+* 123 is the concertid, 50.00 is the price, 456 is the location id is a foreign key
+* 500 is the total capacity of the concert, 100 is the current availability of the concert
+* when creating, the avaialability should be set equal to capacity
+*/
 INSERT INTO Concert
-VALUES(123/*concertid*/, 'concertName', timestamp/*future time*/, 50.00/*price*/, 123/*locationid*/, 500, 100);
+VALUES(123/*concertid*/, 'concertName', timestamp/*future time*/, 50.00/*price*/, 456/*locationid*/, 500, 100);
 
-
--- When a user rates a concert
+/*
+* When a user is going to attend a concert, a new tuple will be created in Attend table
+* 123 is the concertid
+* set the rate and review to be null
+* timestamp recorded the time when insertion happened
+*/
 INSERT INTO Attend
-VALUES('userid',123/*concertid*/, 9/*rating*/, 'review', timestamp);
+VALUES('userid',123/*concertid*/, null, null, timestamp);
 
+/*
+* When a user is rating a concert
+* 9 is the rating of the concert
+* 'review' is the user's review of the concert, and maximum is 500 characters
+*/
+UPDATE Attend
+SET rate = 9, review = 'review'
+WHERE auid = uid 
+	AND acid =cid;
 
--- Display all genre types that the user with a uid XXXXX likes
+/*
+* Display all genre types that the user with a uid XXXXX likes
+*/
 SELECT ggenre
 FROM Likes, Genre
 WHERE Likes.lgenre = Genre.gid
 	AND Likes.luid = 'XXXXX';
 
-
--- Display all concert is going to be held in the user's city
+/*
+* Display all concert is going to be held in the user's city
+*/
 SELECT cname
 FROM Concert, Location, User
 WHERE User.ucity = Location.city
