ffcfc2b8c784f3b329151c75b2f2ee000b11fd06
southpenguin <southpenguin@gmail.com>
2014-12-03 00:27:13 -0500

    Update index.php

--- php/index.php
+++ php/index.php
@@ -1,23 +1,42 @@
 <?php
-    //require_once 'includes/Query-class.php';
-
     session_start();
-    
-    //$user = $dbquery->LoadUserObj($_SESSION["UID"]);
-    
     if(!isset($_SESSION["UID"])){
-        header("Location:login.php");
+        header("Location:entry.php");
     }
-    else $user = $_SESSION["UID"];
+    else {
+        $uid = $_SESSION["UID"];
+        $username = $_SESSION["Username"];
+        $ufname = $_SESSION["FirstName"];
+    }
 ?>
-
 
 <!DOCTYPE html>
 <html>
     <head>
-        
+        <meta charset="UTF-8">
+        <title>Home</title>
+        <link rel ="stylesheet" href="css/style.css">
     </head>
     <body>
-        adsfasdf
+        <div id ="navigation">
+            <div id="topcontent">
+                <div id="logo">
+                    <a href="index.php">Concert</a>
+                </div>
+                
+                <div id="hometop">
+                    <ul>
+                        <li><a href = "index.php">Home</a></li>
+                        <li><a href = "artists.php">Artists</a></li>
+                        <li><a href = "concerts.php">Concerts</a></li>
+                        <li><a href = "connection.php">Connection</a></li>
+                        <li><a href = "profile.php">My Profile</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="center">
+            Welcome Back, <?php echo $ufname;?>
+        </div>
     </body>
 </html>
