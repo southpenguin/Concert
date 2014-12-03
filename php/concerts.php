2ec505c6fcb42bf42b5e1513e71ab74a02cabcb2
southpenguin <southpenguin@gmail.com>
2014-12-03 00:31:46 -0500

    Create concerts.php

---
+++ php/concerts.php
@@ -0,0 +1,53 @@
+<?php
+session_start();
+
+if (!isset($_SESSION["UID"])) {
+    header("Location:entry.php");
+} else {
+    $user = $_SESSION["UID"];
+    $username = $_SESSION["Username"];
+    $ufname = $_SESSION["FirstName"];
+}
+?>
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Home Page</title>
+        <link rel ="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <div id ="navigation">
+            <div id="topcontent">
+                <div id="logo">
+                    <a href="index.php">Concert</a>
+                </div>
+
+                <div id="hometop">
+                    <ul>
+                        <li><a href = "index.php">Home</a></li>
+                        <li><a href = "artists.php">Artists</a></li>
+                        <li><a href = "concerts.php">Concerts</a></li>
+                        <li><a href = "connection.php">Connection</a></li>
+                        <li><a href = "profile.php">My Profile</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="center">
+            <div id="user">
+                Welcome Back, <?php echo $ufname; ?>
+            </div>
+            <div class="left">
+
+            </div>
+
+            <div class="right">
+
+            </div>
+
+        </div>
+    </body>
+</html>
