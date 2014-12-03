3d89f125a9af3c4aac79d3996aa9cef7879f449c
rean.wen <reaniwen@gmail.com>
2014-12-03 01:12:17 -0500

    Update Sample Data

--- sql/SampleData.sql
+++ sql/SampleData.sql
@@ -1,23 +1,23 @@
 
 -- User
-insert into user values('aaa','123456','Peter','Allen','P.Allen@gmail.com','New York','6461234567');
-insert into user values('tomlee','654321','Tom','Lee','tomlee@yahoo.com','New York','7181234567');
-insert into user values('lovemusic','lovemusic','Alice','Julia','lovemusic@gmail.com','Brooklyn','7185455678');
-insert into user values('user','1234567','Aaron','Joe','joe123@aol.com','Queens','718378265');
-insert into user values('alex123','7654321','Alexander','Thomas','alextom@nyu.edu','New York','7185639825');
-insert into user values('westcoast','wcoast','Allen','James','westcoast@gmail.com','Los Angeles','3267548264');
-insert into user values('catcat','catcat','Catty','Petty','cptty@gmail.com','Los Angeles','3267149478');
-insert into user values('dogdog','doggy123','Dom','Kevin','domkevin@gmail.com','Boston','6738261749');
+insert into user values(null,'aaa','123456','Peter','Allen','P.Allen@gmail.com','New York','6461234567', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0);
+insert into user values(null,'tomlee','654321','Tom','Lee','tomlee@yahoo.com','New York','7181234567', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0);
+insert into user values(null,'lovemusic','lovemusic','Alice','Julia','lovemusic@gmail.com','Brooklyn','7185455678', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0);
+insert into user values(null,'user','1234567','Aaron','Joe','joe123@aol.com','Queens','718378265', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0);
+insert into user values(null,'alex123','7654321','Alexander','Thomas','alextom@nyu.edu','New York','7185639825', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0);
+insert into user values(null,'westcoast','wcoast','Allen','James','westcoast@gmail.com','Los Angeles','3267548264', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0);
+insert into user values(null,'catcat','catcat','Catty','Petty','cptty@gmail.com','Los Angeles','3267149478', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0);
+insert into user values(null,'dogdog','doggy123','Dom','Kevin','domkevin@gmail.com','Boston','6738261749', CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0);
 
 -- Arts
-insert into art values('linkinpark','12345678','Linkin Park','admin@linkinpark.com','www.linkinpark.com');
-insert into art values('jblunt','12345678','James Blunt','jblant@gmail.com','www.jamesblunt.com');
-insert into art values('lenka','12345678','Lenka','lenka@sina.com','www.lenkamusic.com');
-insert into art values('eminem123','12345678','Eminem','eminem@eminem.com','www.eminem.com');
-insert into art values('2hrose','12345678','Second Hand Roses','ershoumeigui@sina.com','http://site.douban.com/ershoumeigui/');
-insert into art values('sum14','12345678','Sum41','sum41@myspace.com','www.sum41.com');
-insert into art values('oasis1990','12345678','Oasis','oasis1990@gmail.com','www.oasis.com');
-insert into art values('backdoor','12345678','Back Door','backdoor@gmail.com','www.facebbok.com/backdoor');
+insert into art values(null,'linkinpark','12345678','Linkin Park','admin@linkinpark.com','www.linkinpark.com', CURRENT_TIMESTAMP);
+insert into art values(null,'jblunt','12345678','James Blunt','jblant@gmail.com','www.jamesblunt.com', CURRENT_TIMESTAMP);
+insert into art values(null,'lenka','12345678','Lenka','lenka@sina.com','www.lenkamusic.com', CURRENT_TIMESTAMP);
+insert into art values(null,'eminem123','12345678','Eminem','eminem@eminem.com','www.eminem.com', CURRENT_TIMESTAMP);
+insert into art values(null,'2hrose','12345678','Second Hand Roses','ershoumeigui@sina.com','http://site.douban.com/ershoumeigui/', CURRENT_TIMESTAMP);
+insert into art values(null,'sum14','12345678','Sum41','sum41@myspace.com','www.sum41.com', CURRENT_TIMESTAMP);
+insert into art values(null,'oasis1990','12345678','Oasis','oasis1990@gmail.com','www.oasis.com', CURRENT_TIMESTAMP);
+insert into art values(null,'backdoor','12345678','Back Door','backdoor@gmail.com','www.facebbok.com/backdoor', CURRENT_TIMESTAMP);
 
 -- Location
 insert into location values('0001','Barkley Center',620,'Atlantic Avenue',NULL,'Brooklyn','NY','11217');
