9ca84213d37fb2645153c9dcaeb78f88e27b65ba
southpenguin <southpenguin@gmail.com>
2014-12-05 00:59:58 -0500

    Create Navigation.php

---
+++ php/includes/Navigation.php
@@ -0,0 +1,20 @@
+
+</head>
+    <body>
+        <div id ="navigation">
+            <div id="topcontent">
+                <div id="logo">
+                    <a href="index.php">Concert</a>
+                </div>
+
+                <div id="hometop">
+                    <ul>
+                        <li><a href = "index.php">Home</a></li>
+                        <li><a href = "artists.php">Artists</a></li>
+                        <li><a href = "concerts.php">Concerts</a></li>
+                        <li><a href = "connection.php">Connection</a></li>
+                        <li><a href = "profile.php">My Profile</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
