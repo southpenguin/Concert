63699fa45ac545e6e4ce7c2c67bfd8e13e3b487c
southpenguin <southpenguin@gmail.com>
2014-12-05 01:10:59 -0500

    Update concerts.php

--- php/concerts.php
+++ php/concerts.php
@@ -1,53 +1,21 @@
-<?php
-session_start();
+<?php include 'includes/Head.php';?>
 
-if (!isset($_SESSION["UID"])) {
-    header("Location:entry.php");
-} else {
-    $user = $_SESSION["UID"];
-    $username = $_SESSION["Username"];
-    $ufname = $_SESSION["FirstName"];
-}
-?>
+<title>Concerts</title>
+<link rel ="stylesheet" href="css/style.css">
 
+<?php include 'includes/Navigation.php';?>
+<div class="center">
+    <div id="user">
+        Welcome Back, <?php echo $ufname; ?>
+    </div>
+    <div class="left">
 
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Home Page</title>
-        <link rel ="stylesheet" href="css/style.css">
-    </head>
-    <body>
-        <div id ="navigation">
-            <div id="topcontent">
-                <div id="logo">
-                    <a href="index.php">Concert</a>
-                </div>
+    </div>
 
-                <div id="hometop">
-                    <ul>
-                        <li><a href = "index.php">Home</a></li>
-                        <li><a href = "artists.php">Artists</a></li>
-                        <li><a href = "concerts.php">Concerts</a></li>
-                        <li><a href = "connection.php">Connection</a></li>
-                        <li><a href = "profile.php">My Profile</a></li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-        <div class="center">
-            <div id="user">
-                Welcome Back, <?php echo $ufname; ?>
-            </div>
-            <div class="left">
+    <div class="right">
 
-            </div>
+    </div>
 
-            <div class="right">
-
-            </div>
-
-        </div>
-    </body>
+</div>
+</body>
 </html>
